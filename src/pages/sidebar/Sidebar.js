cb6d1796b6f2da5c1e36b4ee23ad93a6823ed9e5
fdcoder81 <carloanselmi81@gmail.com>
2020-11-19 17:47:51 +0000

    first commit

---
+++ src/pages/sidebar/Sidebar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+  FaUser,
+  FaDesktop,
+  FaBuilding,
+  FaCode,
+  FaUniversity,
+  FaEnvelope,
+} from "react-icons/fa";
+import "./sidebar.scss";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-title">
+        <h1>Carlo Anselmi</h1>
+        <h2>Front end Developer</h2>
+      </div>
+      <div className="sidebar-menu">
+        <div className="sidebar-item">
+          <FaUser size="20" color="#00008b" />
+          <p>About</p>
+        </div>
+        <div className="sidebar-item">
+          <FaBuilding size="20" color="#00008b" />
+          <p>Experience</p>
+        </div>
+        <div className="sidebar-item">
+          <FaDesktop size="20" color="#00008b" />
+          <p>Projects</p>
+        </div>
+        <div className="sidebar-item">
+          <FaCode size="20" color="#00008b" />
+          <p>Skills</p>
+        </div>
+        <div className="sidebar-item">
+          <FaUniversity size="20" color="#00008b" />
+          <p>Education</p>
+        </div>
+        <div className="sidebar-item">
+          <FaEnvelope size="20" color="#00008b" />
+          <p>Contact</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
