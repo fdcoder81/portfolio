376f5a7518c2aceeac3e9a47add1e88f7ca57e4c
fdcoder81 <carloanselmi81@gmail.com>
2020-11-24 23:25:47 +0000

    Experience card header

--- src/component/experience-card/ExperienceCard.js
+++ src/component/experience-card/ExperienceCard.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./experienceCard.scss";
 
+import desk from "../../img/desk.png";
+
 function ExperienceCard() {
-  return <div>EXPERIENCE CARD</div>;
+  return (
+    <div className="experience-card">
+      <div className="card-header">
+        <div className="company">
+          <img src={desk} alt="" />
+          <p className="company-title">Freelancer</p>
+        </div>
+        <p className="card-header-position">Front End developer</p>
+      </div>
+      <p className="date">Jun 2020 - Present | London, UK</p>
+    </div>
+  );
 }
 
 export default ExperienceCard;
